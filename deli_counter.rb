e518c094923ae81f0ee9d0093fab0daa958cf4ac
David Mills <david_mills109@yahoo.com>
2020-09-15 23:36:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,52 +1,30 @@
 # Write your code here.
-katz_deli = ["Moshe", "Fayge", "Rivki"]
+def take_a_number(katz_deli, name)
+  katz_deli << name
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
 
-
-def line(array) # this was the one I figured out
-  if array.length >= 1
-    nuarray = []
-    counter = 1 
-    array.each do |name|
-      nuarray.push("#{counter}. #{name}")
-      counter += 1 
-    end 
-    puts "The line is currently: #{nuarray.join(" ")}"
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
   else
+    puts "Currently serving #{katz_deli.shift}."
+  end
+
+end
+
+def line(katz_deli)
+  if katz_deli.length == 0
     puts "The line is currently empty."
-  end
-end
-
-line(katz_deli)
-
-def line_simple(array) # this one follows the same mechanics as learn.co
-  current_line = "The simple line is currently:"
-  array.each.with_index(1) do |value, indexemus|  
-  # "each.with_index" is the method...must use "index"
-    current_line << " #{indexemus}. #{value},"    
-    # "indexemus" is used to illustrate this variable can be different from "index"
-  end 
-  puts current_line
-end 
-  
-line_simple(katz_deli)  
-#[:foo, :bar, :baz].each.with_index(1) do |value, index|
-#    puts "#{index}: #{value}"
-#end
-
-
-
-def take_a_number(line, new_person)
-  line.push(new_person) # could say: "line << new_person"
-  puts "Welcome, #{new_person}. You are number #{line.length} in line."
-end
-
-take_a_number(katz_deli, "Fyvish")
-
-def now_serving(line)
-  if line.length == 0 # could say: "if deli.empty?"
-    puts"There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}." # could say: "Currently serving #{line.first}."
-    line.shift # this works in the IDE but no on repl.it
+    message="The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
+    end
+
+    puts "#{message}"
   end
+
+
 end
